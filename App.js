27ec42c042df557301c0e69add6bd2cfd44fead5
Kobie <spaet062@umn.edu>
2017-12-04 15:06:44 -0800

    React Native setup

--- App.js
+++ App.js
@@ -1,70 +1,21 @@
 import React from 'react';
-import {
-  StyleSheet,
-  View,
-  Text,
-  TouchableOpacity,
-  TextInput,
-  ListView,
-  Alert,
-  Button,
-  RefreshControl,
-  AsyncStorage,
-} from 'react-native';
-import {
-  StackNavigator,
-} from 'react-navigation';
-import {
-  Location,
-  Permissions,
-  MapView
-} from 'expo';
+import { StyleSheet, Text, View } from 'react-native';
 
-//Screens
-import SplashScreen from './Screens/SplashScreen';
-import LoginScreen from './Screens/LoginScreen';
-import RegisterScreen from './Screens/RegisterScreen';
-import UsersScreen from './Screens/UsersScreen';
-import MessagesScreen from './Screens/MessagesScreen';
-import ConversationScreen from './Screens/ConversationScreen';
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
+}
 
-//Navigator
-export default StackNavigator({
-  Splash: {
-    screen: SplashScreen,
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  Login: {
-    screen: LoginScreen,
-  },
-  Register: {
-    screen: RegisterScreen,
-  },
-  Users: {
-    screen: UsersScreen,
-  },
-  Messages: {
-    screen: MessagesScreen,
-  },
-  Conversation: {
-    screen: ConversationScreen,
-  },
-}, {initialRouteName: 'Splash'});
-
-// var portscanner = require('portscanner')
-//
-// var ip = require("ip");
-// var ipheader = ip.address().split(".")
-// ipheader.pop();
-//
-// var ipheader = ipheader.join(".")+"."
-//
-// var promiseArr = [];
-// for (var i = 1; i < 254; i ++){
-//   promiseArr.push(portscanner.checkPortStatus(8228, ipheader+i));
-// }
-//
-// Promise.all(promiseArr)
-// .then( (x) => x.map( (y,i) => {
-//   if(y === "open") console.log(ipheader+(i+1));
-// }))
-// .catch(err => console.log(err))
+});
