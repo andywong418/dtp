8bbd173024f9a9f5407bf68cbeb2970f35736d61
obadiar <ob.diar@gmail.com>
2017-12-07 19:08:12 -0800

    Starting chat rooms

--- backend/models/User.js
+++ backend/models/User.js
@@ -34,6 +34,7 @@ const UserSchema = new Schema({
     photos: [{}],
     friends: [{}],
     purpose: String,
+    bio: String,
     profileComplete: Boolean
 })
 
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,11 +2,14 @@ const express = require('express');
 const router = express.Router();
 const facebookAPI = require('./facebook_api');
 const interestsAPI = require('./interests');
+const chatroomsAPI = require('./chatrooms');
 
 /* GET home page. */
 router.use('/facebook', facebookAPI);
 
 router.use('/interests', interestsAPI);
+
+router.use('/chatrooms', chatroomsAPI);
 
 router.get('/', function (req, res, next) {
   res.send("Received");
