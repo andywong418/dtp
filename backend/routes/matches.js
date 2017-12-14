9b6e284e24191542b5a0aba725eec7a69e70a0c1
obadiar <ob.diar@gmail.com>
2017-12-14 15:49:23 -0800

    Match routes

---
+++ backend/routes/matches.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const Match = require('../models/Match');
+
+router.post('/updateMatchResponse', (req, res) => {
+	let { personA, personB, response } = req.body;
+	Match.findOne({ personA: personB, personB: personA }, (error, match) => {
+		if (error) {
+			console.log('Error finding match:', error);
+		} else if (!match) {	//This person is swiping first
+			Match.create({ personA, personB, response }, (error, match) => {
+				if (error) {
+					console.log('Error creating match:', error);
+					res.send('Error creating match:', error);
+				} else {
+					console.log('Match created successfully!');
+					res.send('Match created successfully!');
+				}
+			})
+		} else {	//If other person has swiped on them
+			if (match.response && response) {	// * Both said yes
+				Match.update({ matched: true }, (error, match) => {
+					if (error) {
+						console.log('Error updating match:', error);
+						res.send('Error updating match:', error);
+					} else {
+						console.log('It\'s a match!');
+						res.send('It\'s a match!');
+					}
+				})
+			} else {
+				Match.update({ matched: false }, (error, match) => {
+					if (error) {
+						console.log('Error updating match:', error);
+						res.send('Error updating match:', error);
+					} else {
+						console.log('No match!');
+						res.send('No match!');
+					}
+				})
+			}
+		}
+	})
+})
+
+module.exports = router;
